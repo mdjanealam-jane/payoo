df50ecef233c8030e9b3e0180b2d1c29c2e236d8
mdjanealam-jane <mdjanealam.mdja@gmail.com>
2026-02-21 10:18:58 +0600

    login page designed and functionality added

---
+++ js logic/login.js
@@ -0,0 +1,22 @@
+document.getElementById("login-btn").addEventListener("click", function (){
+    // number
+    const inputNumber= document.getElementById("number-input");
+    const contactNumber= inputNumber.value;
+    console.log("contactNumber");
+
+    // pin
+    const inputPin= document.getElementById("pin-input");
+    const pin= inputPin.value;
+    console.log("pin");
+
+    // login logic
+    if(contactNumber==01234567890 && pin==1234){
+        alert("Login Success");
+        window.location.assign("/home.html")
+    }
+    else{
+        alert("Login Failed");
+    }
+
+})
+
